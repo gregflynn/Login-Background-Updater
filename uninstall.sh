a4ae80132ec9881332c2c0489828066ab6da6272
Greg Flynn <gflynn1@binghamton.edu>
2011-07-18 21:40:04 -0400

    added an uninstaller if you hate this! (requires admin)

---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+rm /System/Library/Frameworks/AppKit.framework/Versions/C/Resources/NSTexturedFullScreenBackgroundColor.png
+mv /System/Library/Frameworks/AppKit.framework/Versions/C/Resources/NSTexturedFullScreenBackgroundColor_original.png /System/Library/Frameworks/AppKit.framework/Versions/C/Resources/NSTexturedFullScreenBackgroundColor.png
+rm /.LoginBackground
